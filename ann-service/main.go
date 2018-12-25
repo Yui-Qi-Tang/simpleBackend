77c36757eeec8dc2fbd0f22412b65e7d158739ea
Yui Qi Tang <yqtang1222@gmail.com>
2018-12-25 16:04:59 +0800

    add main.go for ann-service

---
+++ ann-service/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"simpleBackend/ann-service/pianogame"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func main() {
+	/*
+		TO-DO:
+			1. load config from file for mongodb
+			    Base data: host/port/connect protocol
+				Q: specify db and collection, in mongdb.go??
+			2. create a Mode 
+	*/
+	/* ann-service here */
+	/* Go-Gin setup */
+	gin.SetMode(gin.TestMode) // enable server on localhost:8080
+	router := gin.Default()
+	router.LoadHTMLGlob("pianogame/templates/*") // load tempates
+
+	/* APIs */
+	router.POST("user/login", pianogame.UserLogin)  // login
+	router.POST("user/register", pianogame.UserRegister) // signup
+
+    /* Web page */
+	router.GET("/login", pianogame.LoginPage) // login page
+	router.GET("/signup", pianogame.SignupPage)  // signup page
+	
+	/* Run server */
+	log.Println("Start server")
+	router.Run() // listen and serve on 127.0.0.1:8080 in gin.TestMode
+}
