ce737d3e3eceb833dc23f421f841bad441570722
yqtang <yqtang1222@gmail.com>
2019-01-20 17:06:22 +0800

    compress result

--- file/main.go
+++ file/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -25,31 +26,66 @@ func readFile(filePath string) *os.File {
 }
 
 func main() {
-	filePath := "PUT_YOUR_DATA_PATH"
-	file := readFile(filePath)
+
+	filePath := flag.String("file", "./result", "file path")
+	flag.Parse() // pare variables from commnad line
+	file := readFile(*filePath)
 	defer file.Close()
+
+	var fileStreamErr error
+
+	// no matched songs record
+	var noMatch *os.File
+	noMatch, fileStreamErr = os.Create("./noMatched.log")
+	check(fileStreamErr)
+	defer noMatch.Close() // close file stream
+
 	scanner := bufio.NewScanner(file)
 
 	totalItems := 0
+	matchItems := 0
 	correctHits := 0
 	for scanner.Scan() {
 		totalItems++
 		sText := scanner.Text()
 		sText = strings.Replace(sText, "\n", "", -1)
 		resultList := strings.Split(sText, "+")
+		correctName := ""
+		hitFlag := false
+
+		// check if query answer is in resultList
 		for i, v := range resultList {
 			if i == 0 {
 				// top: [queryName, result, scope]
 				top := strings.Split(v, "\t")
-				if strings.Contains(top[0], top[1]) {
-					// fmt.Println("+1")
+				correctName = top[0]
+				if strings.Contains(correctName, top[1]) {
 					correctHits++
+					hitFlag = true
+				}
+			} else {
+				// otherwise: [result, scope]
+				rankResult := strings.Split(v, "\t")
+				if strings.Contains(correctName, rankResult[0]) {
+					matchItems++
+					hitFlag = true
 				}
 			} //fi
-			// otherwise: [result, scope]
-			// TO-DO check other result?
 		} // for
+		if !hitFlag {
+			data := []byte(correctName)
+			_, err := noMatch.Write(append(data, byte('\n')))
+			check(err)
+			noMatch.Sync()
+		}
 	} // for
 
-	fmt.Println(totalItems, correctHits, float64(correctHits)/float64(totalItems))
+	fmt.Println(
+		totalItems, // song total number
+		correctHits,
+		float64(correctHits)/float64(totalItems), // correct hit rate
+		matchItems, // number of the  correct answer of the query not in top1
+		((correctHits + matchItems) == totalItems), // if true -> queries are correct
+		(correctHits + matchItems), float64(correctHits+matchItems)/float64(totalItems),
+	)
 } // main
