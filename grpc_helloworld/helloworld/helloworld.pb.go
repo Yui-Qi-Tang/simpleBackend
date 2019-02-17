2d068ff7907506ccce7727eb77684d36412175fa
yqtang <yqtang1222@gmail.com>
2019-02-17 10:19:49 +0800

    add my first gRPC function

--- grpc_helloworld/helloworld/helloworld.pb.go
+++ grpc_helloworld/helloworld/helloworld.pb.go
@@ -35,7 +35,7 @@ func (m *HelloRequest) Reset()         { *m = HelloRequest{} }
 func (m *HelloRequest) String() string { return proto.CompactTextString(m) }
 func (*HelloRequest) ProtoMessage()    {}
 func (*HelloRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_helloworld_c7e30385c8e356de, []int{0}
+	return fileDescriptor_helloworld_b0ce01d0f82766ee, []int{0}
 }
 func (m *HelloRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_HelloRequest.Unmarshal(m, b)
@@ -74,7 +74,7 @@ func (m *HelloReply) Reset()         { *m = HelloReply{} }
 func (m *HelloReply) String() string { return proto.CompactTextString(m) }
 func (*HelloReply) ProtoMessage()    {}
 func (*HelloReply) Descriptor() ([]byte, []int) {
-	return fileDescriptor_helloworld_c7e30385c8e356de, []int{1}
+	return fileDescriptor_helloworld_b0ce01d0f82766ee, []int{1}
 }
 func (m *HelloReply) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_HelloReply.Unmarshal(m, b)
@@ -101,9 +101,89 @@ func (m *HelloReply) GetMessage() string {
 	return ""
 }
 
+// My first gRPC request
+type MyRequest struct {
+	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *MyRequest) Reset()         { *m = MyRequest{} }
+func (m *MyRequest) String() string { return proto.CompactTextString(m) }
+func (*MyRequest) ProtoMessage()    {}
+func (*MyRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_helloworld_b0ce01d0f82766ee, []int{2}
+}
+func (m *MyRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_MyRequest.Unmarshal(m, b)
+}
+func (m *MyRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_MyRequest.Marshal(b, m, deterministic)
+}
+func (dst *MyRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MyRequest.Merge(dst, src)
+}
+func (m *MyRequest) XXX_Size() int {
+	return xxx_messageInfo_MyRequest.Size(m)
+}
+func (m *MyRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_MyRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MyRequest proto.InternalMessageInfo
+
+func (m *MyRequest) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+// My first gRPC response
+type MyResponse struct {
+	Message              string   `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *MyResponse) Reset()         { *m = MyResponse{} }
+func (m *MyResponse) String() string { return proto.CompactTextString(m) }
+func (*MyResponse) ProtoMessage()    {}
+func (*MyResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_helloworld_b0ce01d0f82766ee, []int{3}
+}
+func (m *MyResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_MyResponse.Unmarshal(m, b)
+}
+func (m *MyResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_MyResponse.Marshal(b, m, deterministic)
+}
+func (dst *MyResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MyResponse.Merge(dst, src)
+}
+func (m *MyResponse) XXX_Size() int {
+	return xxx_messageInfo_MyResponse.Size(m)
+}
+func (m *MyResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_MyResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MyResponse proto.InternalMessageInfo
+
+func (m *MyResponse) GetMessage() string {
+	if m != nil {
+		return m.Message
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*HelloRequest)(nil), "helloworld.HelloRequest")
 	proto.RegisterType((*HelloReply)(nil), "helloworld.HelloReply")
+	proto.RegisterType((*MyRequest)(nil), "helloworld.MyRequest")
+	proto.RegisterType((*MyResponse)(nil), "helloworld.MyResponse")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -120,6 +200,8 @@ const _ = grpc.SupportPackageIsVersion4
 type GreeterClient interface {
 	// Sends a greeting
 	SayHello(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error)
+	// My first greeting
+	MyHello(ctx context.Context, in *MyRequest, opts ...grpc.CallOption) (*MyResponse, error)
 }
 
 type greeterClient struct {
@@ -139,10 +221,21 @@ func (c *greeterClient) SayHello(ctx context.Context, in *HelloRequest, opts ...
 	return out, nil
 }
 
+func (c *greeterClient) MyHello(ctx context.Context, in *MyRequest, opts ...grpc.CallOption) (*MyResponse, error) {
+	out := new(MyResponse)
+	err := c.cc.Invoke(ctx, "/helloworld.Greeter/MyHello", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GreeterServer is the server API for Greeter service.
 type GreeterServer interface {
 	// Sends a greeting
 	SayHello(context.Context, *HelloRequest) (*HelloReply, error)
+	// My first greeting
+	MyHello(context.Context, *MyRequest) (*MyResponse, error)
 }
 
 func RegisterGreeterServer(s *grpc.Server, srv GreeterServer) {
@@ -167,6 +260,24 @@ func _Greeter_SayHello_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Greeter_MyHello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MyRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GreeterServer).MyHello(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/helloworld.Greeter/MyHello",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GreeterServer).MyHello(ctx, req.(*MyRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Greeter_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "helloworld.Greeter",
 	HandlerType: (*GreeterServer)(nil),
@@ -175,24 +286,31 @@ var _Greeter_serviceDesc = grpc.ServiceDesc{
 			MethodName: "SayHello",
 			Handler:    _Greeter_SayHello_Handler,
 		},
+		{
+			MethodName: "MyHello",
+			Handler:    _Greeter_MyHello_Handler,
+		},
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "helloworld.proto",
 }
 
-func init() { proto.RegisterFile("helloworld.proto", fileDescriptor_helloworld_c7e30385c8e356de) }
+func init() { proto.RegisterFile("helloworld.proto", fileDescriptor_helloworld_b0ce01d0f82766ee) }
 
-var fileDescriptor_helloworld_c7e30385c8e356de = []byte{
-	// 174 bytes of a gzipped FileDescriptorProto
+var fileDescriptor_helloworld_b0ce01d0f82766ee = []byte{
+	// 214 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0x12, 0xc8, 0x48, 0xcd, 0xc9,
 	0xc9, 0x2f, 0xcf, 0x2f, 0xca, 0x49, 0xd1, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x42, 0x88,
 	0x28, 0x29, 0x71, 0xf1, 0x78, 0x80, 0x78, 0x41, 0xa9, 0x85, 0xa5, 0xa9, 0xc5, 0x25, 0x42, 0x42,
 	0x5c, 0x2c, 0x79, 0x89, 0xb9, 0xa9, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0x9c, 0x41, 0x60, 0xb6, 0x92,
 	0x1a, 0x17, 0x17, 0x54, 0x4d, 0x41, 0x4e, 0xa5, 0x90, 0x04, 0x17, 0x7b, 0x6e, 0x6a, 0x71, 0x71,
-	0x62, 0x3a, 0x4c, 0x11, 0x8c, 0x6b, 0xe4, 0xc9, 0xc5, 0xee, 0x5e, 0x94, 0x9a, 0x5a, 0x92, 0x5a,
-	0x24, 0x64, 0xc7, 0xc5, 0x11, 0x9c, 0x58, 0x09, 0xd6, 0x25, 0x24, 0xa1, 0x87, 0xe4, 0x02, 0x64,
-	0xcb, 0xa4, 0xc4, 0xb0, 0xc8, 0x00, 0xad, 0x50, 0x62, 0x70, 0x32, 0xe0, 0x92, 0xce, 0xcc, 0xd7,
-	0x4b, 0x2f, 0x2a, 0x48, 0xd6, 0x4b, 0xad, 0x48, 0xcc, 0x2d, 0xc8, 0x49, 0x2d, 0x46, 0x52, 0xeb,
-	0xc4, 0x0f, 0x56, 0x1c, 0x0e, 0x62, 0x07, 0x80, 0xbc, 0x14, 0xc0, 0x98, 0xc4, 0x06, 0xf6, 0x9b,
-	0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x0f, 0xb7, 0xcd, 0xf2, 0xef, 0x00, 0x00, 0x00,
+	0x62, 0x3a, 0x4c, 0x11, 0x8c, 0xab, 0x24, 0xcf, 0xc5, 0xe9, 0x5b, 0x49, 0xc0, 0x20, 0x90, 0x82,
+	0xe2, 0x82, 0xfc, 0xbc, 0xe2, 0x54, 0xdc, 0x06, 0x19, 0xb5, 0x32, 0x72, 0xb1, 0xbb, 0x17, 0xa5,
+	0xa6, 0x96, 0xa4, 0x16, 0x09, 0xd9, 0x71, 0x71, 0x04, 0x27, 0x56, 0x82, 0xed, 0x17, 0x92, 0xd0,
+	0x43, 0xf2, 0x0b, 0xb2, 0xb3, 0xa5, 0xc4, 0xb0, 0xc8, 0x00, 0x1d, 0xab, 0xc4, 0x20, 0x64, 0xc5,
+	0xc5, 0xee, 0x0b, 0xd5, 0x2e, 0x8a, 0xac, 0x08, 0xee, 0x52, 0x54, 0xbd, 0x08, 0xf7, 0x29, 0x31,
+	0x38, 0x19, 0x70, 0x49, 0x67, 0xe6, 0xeb, 0xa5, 0x17, 0x15, 0x24, 0xeb, 0xa5, 0x56, 0x24, 0xe6,
+	0x16, 0xe4, 0xa4, 0x16, 0x23, 0xa9, 0x75, 0xe2, 0x07, 0x1b, 0x1b, 0x0e, 0x62, 0x07, 0x80, 0x02,
+	0x36, 0x80, 0x31, 0x89, 0x0d, 0x1c, 0xc2, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x17, 0x12,
+	0x69, 0x75, 0x75, 0x01, 0x00, 0x00,
 }
---
+++ grpc_helloworld/simple_build.sh
@@ -0,0 +1,2 @@
+# !/bin/sh
+protoc -I helloworld/ helloworld/helloworld.proto --go_out=plugins=grpc:helloworld
